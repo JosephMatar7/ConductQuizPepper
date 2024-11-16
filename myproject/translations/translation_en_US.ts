2d800d70ee63551d48006743b3a7d7be5ac2811b
JosephMatar7 <joseph.m.mattar@net.usek.edu.lb>
2024-11-16 11:21:48 +0200

    Add files via upload

---
+++ myproject/translations/translation_en_US.ts
@@ -0,0 +1,18 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+    <context>
+        <name>behavior_1/behavior.xar:/Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Time is Over, Don't be sad</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Time is Over, Don't be sad</translation>
+        </message>
+    </context>
+</TS>
